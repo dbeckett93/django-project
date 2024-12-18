79e160286f7f787155579e7fd1b3ad940b4c5dc3
Daniel Beckett <dan-beckett@hotmail.co.uk>
2024-12-18 11:34:50 +0000

    Add: Gunicorn and Procfile for Heroku deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@h*whhuf+@qkvqztqtodzbr1t7zsngar%fu+q5g6$(e8@+ujue'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
